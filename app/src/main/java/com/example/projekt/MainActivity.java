0755812dffe67659b0857c217ef39d5e20f32153
Filip <filipiak.filip96@gmail.com>
2020-04-17 13:31:37 +0200

    komentarz z odczytywaniem

--- app/src/main/java/com/example/projekt/MainActivity.java
+++ app/src/main/java/com/example/projekt/MainActivity.java
@@ -3,14 +3,54 @@ package com.example.projekt;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //super komentarz
+
+
+
+
+
+      
+
+
+
+
+
+
+
+       /* DatabaseReference reference = FirebaseDatabase.getInstance().getReference().child("Przepisy");
+
+        reference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                for (DataSnapshot snapshot : dataSnapshot.getChildren())
+                {
+
+                    textViewOpis.setText(snapshot.getValue().toString());
+
+                }
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                textViewOpis.setText(databaseError.getMessage().toString());
+            }
+        }); */
 
     }
 }
