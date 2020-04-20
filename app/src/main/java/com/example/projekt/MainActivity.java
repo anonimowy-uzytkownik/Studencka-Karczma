1cd2c5e9db417b10bb3a27745cdec884e31b820c
MakKolb <hondoc98@gmail.com>
2020-04-20 19:15:32 +0200

    Opis i składniki przykład

--- app/src/main/java/com/example/projekt/MainActivity.java
+++ app/src/main/java/com/example/projekt/MainActivity.java
@@ -7,16 +7,13 @@ import android.widget.TextView;
 
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
 
-import org.w3c.dom.Text;
-
 public class MainActivity extends AppCompatActivity {
 
-    TextView textViewOpis ;
+    TextView textViewSkładniki, textViewOpis;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,9 +21,8 @@ public class MainActivity extends AppCompatActivity {
         //super komentarz
 
 
-    textViewOpis=findViewById(R.id.textView);
-
-
+    textViewSkładniki =findViewById(R.id.textViewSkładniki);
+    textViewOpis = findViewById(R.id.textViewOpis);
 
 
 
@@ -37,6 +33,7 @@ public class MainActivity extends AppCompatActivity {
 
        // DatabaseReference reference = FirebaseDatabase.getInstance().getReference().child("przepisy");
         Query reference = FirebaseDatabase.getInstance().getReference().child("przepisy").child("2").child("składniki");
+        Query opis = FirebaseDatabase.getInstance().getReference().child("przepisy").child("2").limitToFirst(1);
 
         reference.addValueEventListener(new ValueEventListener() {
             @Override
@@ -44,12 +41,26 @@ public class MainActivity extends AppCompatActivity {
                 for (DataSnapshot snapshot : dataSnapshot.getChildren())
                 {
 
-                    textViewOpis.setText(snapshot.getValue().toString());
-
+                   // textViewSkładniki.setText(snapshot.getValue().toString());
+                    textViewSkładniki.append(snapshot.getValue().toString()+"\n");
                 }
-
             }
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                textViewSkładniki.setText(databaseError.getMessage().toString());
+            }
+        });
 
+        opis.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                for (DataSnapshot snapshot : dataSnapshot.getChildren())
+                {
+
+                    // textViewSkładniki.setText(snapshot.getValue().toString());
+                    textViewOpis.append(snapshot.getValue().toString()+"\n");
+                }
+            }
             @Override
             public void onCancelled(DatabaseError databaseError) {
                 textViewOpis.setText(databaseError.getMessage().toString());
