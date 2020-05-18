ed4b07940fc53dbe1b15f7f91cf1535a41a01d0f
Filip <filipiak.filip96@gmail.com>
2020-05-18 11:35:40 +0200

    Potwierdzanie mejla

--- app/src/main/java/com/example/projekt/RejestracjaActivity.java
+++ app/src/main/java/com/example/projekt/RejestracjaActivity.java
@@ -16,6 +16,7 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class RejestracjaActivity extends AppCompatActivity {
 
@@ -61,7 +62,8 @@ public class RejestracjaActivity extends AppCompatActivity {
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
                         if (task.isSuccessful()) {
-                            Toast.makeText(RejestracjaActivity.this, "Konto Stworzone", Toast.LENGTH_SHORT).show();
+                            sendVerificationEmail();
+                            Toast.makeText(RejestracjaActivity.this, "Konto stworzone, prosze potwierdzic adres email!", Toast.LENGTH_SHORT).show();
                             startActivity(new Intent(RejestracjaActivity.this, LoginActivity.class));
                         } else {
                             Toast.makeText(RejestracjaActivity.this, "Error! " + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
@@ -71,5 +73,38 @@ public class RejestracjaActivity extends AppCompatActivity {
             }
         });
 
+    }
+
+
+    private void sendVerificationEmail()
+    {
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+
+        user.sendEmailVerification()
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        if (task.isSuccessful()) {
+                            // email sent
+
+
+                            // after email is sent just logout the user and finish this activity
+                            FirebaseAuth.getInstance().signOut();
+                            startActivity(new Intent(RejestracjaActivity.this, LoginActivity.class));
+                            finish();
+                        }
+                        else
+                        {
+                            // email not sent, so display message and restart the activity or do whatever you wish to do
+
+                            //restart this activity
+                            overridePendingTransition(0, 0);
+                            finish();
+                            overridePendingTransition(0, 0);
+                            startActivity(getIntent());
+
+                        }
+                    }
+                });
     }
 }
