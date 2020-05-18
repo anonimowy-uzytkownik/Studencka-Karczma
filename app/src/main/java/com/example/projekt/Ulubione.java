2fac0095889431f2dd0d47dd0febb5d013d64b86
Pepli <peploch27@gmai.com>
2020-05-18 11:35:33 +0200

    Strona główna przepis

--- app/src/main/java/com/example/projekt/Ulubione.java
+++ app/src/main/java/com/example/projekt/Ulubione.java
@@ -2,14 +2,56 @@ package com.example.projekt;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class Ulubione extends AppCompatActivity {
+    Button buttonPrzepisy,buttonKonto,buttonKupony;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ulubione);
+        ListView mListView=(ListView) findViewById(R.id.listView2);
+        buttonPrzepisy=findViewById(R.id.buttonPrzepisy);
+        buttonKonto=findViewById(R.id.buttonKonto);
+        buttonKupony=findViewById(R.id.buttonKupony);
+        final ArrayList<Przepis> przepisList = new ArrayList<>();
+
+        buttonPrzepisy.setOnClickListener(new View.OnClickListener() {
+
+
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Ulubione.this, MainActivity.class));
+            }
+        });
+        buttonKonto.setOnClickListener(new View.OnClickListener() {
+
+
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Ulubione.this, Konto.class));
+            }
+        });
+        buttonKupony.setOnClickListener(new View.OnClickListener() {
+
+
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Ulubione.this, Kupony.class));
+            }
+        });
+
+
+        final PrzepisListAdapter adapter= new PrzepisListAdapter(this,R.layout.adapter_view_przepis,przepisList);
+        mListView.setAdapter(adapter);
+
     }
 
 
