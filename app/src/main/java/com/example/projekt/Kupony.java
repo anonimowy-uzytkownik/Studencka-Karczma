67b66a88c1527456f61af00e7ff382a1cf13fca0
Pepli <peploch27@gmai.com>
2020-05-19 18:11:08 +0200

    Strona główna przepis

--- app/src/main/java/com/example/projekt/Kupony.java
+++ app/src/main/java/com/example/projekt/Kupony.java
@@ -2,13 +2,54 @@ package com.example.projekt;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class Kupony extends AppCompatActivity {
-
+    Button buttonPrzepisy,buttonKonto,buttonUlubione;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_kupony);
+
+        ListView mListView=(ListView) findViewById(R.id.listView3);
+        buttonPrzepisy=findViewById(R.id.buttonPrzepisy);
+        buttonKonto=findViewById(R.id.buttonKonto);
+        buttonUlubione=findViewById(R.id.buttonUlubione);
+
+
+        buttonPrzepisy.setOnClickListener(new View.OnClickListener() {
+
+
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Kupony.this, MainActivity.class));
+            }
+        });
+        buttonKonto.setOnClickListener(new View.OnClickListener() {
+
+
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Kupony.this, Konto.class));
+            }
+        });
+        buttonUlubione.setOnClickListener(new View.OnClickListener() {
+
+
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Kupony.this, Ulubione.class));
+            }
+        });
+
+
+
+
     }
 }
